c9957a84891fcd8487ebd1457c1654626845100f
pro-dev-ryan <azizul.developer@gmail.com>
2022-10-27 16:00:08 +0600

    cors integrated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 const sidebarData = require("./data/sidebar.json");
 const courses = require("./data/courseCard.json");
+const courseDetails = require("./data/courseDetail.json");
+app.use(cors);
 app.get("/", (req, res) => {
   res.send("app is runinng");
 });
@@ -15,13 +18,13 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-// app.get("/courses/:id", (req, res) => {
-//   const id = req.params.id;
-//   console.log(req.params.id);
-//   const singleCourse = courses.filter((course) => course.id === id);
-//   res.send(singleCourse);
-//   console.log(singleCourse);
-// });
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.params.id);
+  const singleCourse = courseDetails.filter((course) => course.id === id);
+  res.send(singleCourse);
+  console.log(singleCourse);
+});
 
 app.listen(port, () => {
   console.log("app is running on", port);
